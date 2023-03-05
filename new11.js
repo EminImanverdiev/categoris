e1104bce06d0779d338a517e596c0f6553b7c356
ITDevelopergithub <imanverdiyevemin414@gmail.com>
2023-03-05 20:55:24 +0400

    first commit

---
+++ new11.js
@@ -0,0 +1,6 @@
+function foradd() {
+    var blur=document.getElementById('blur');
+    blur.classList.toggle('active');
+    var modal=document.getElementsByClassName('modal')[0];
+    modal.classList.toggle('active');
+ }
